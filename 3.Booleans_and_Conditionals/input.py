9449970cc8c631335a4d22127df145b663915c7b
acemouty <kyleguerrero85@gmail.com>
2020-05-16 13:32:44 -0500

    feat: added input file

---
+++ 3.Booleans_and_Conditionals/input.py
@@ -0,0 +1,15 @@
+def main():
+
+    # we can take in user input using
+    # the input function
+    data = input("What is your favorite color?\n")
+    print(f"You said your favorite color is {data}")
+
+    # you can also setup input like this
+    print("what is your favorite food?")
+    fav_food = input()
+    print(f"So your fav food is {fav_food}")
+
+
+if __name__ == "__main__":
+    main()
