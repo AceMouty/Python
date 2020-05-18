05c0ede4b765486d7e1bb7d28aa82d635ad0d496
acemouty <kyleguerrero85@gmail.com>
2020-05-18 18:33:05 -0500

    feat: created basic looping
    created basic looping file that demos how basic
    for looping works along with how to loop over
    a range of numbers

---
+++ 4.Loops/basic_looping.py
@@ -0,0 +1,32 @@
+
+# for loop allows us to do something over
+# and over again
+
+# structure of for loop
+# for item in collection:
+#   print(item)
+
+# print every letter in a word
+for char in "hello world":
+    print(char)
+
+# range: used to generate a range
+# of numbers to help with looping
+# in for loops.
+# the range generated up to but not
+# including the number passed
+
+# range(start, end, skip_count_by)
+# start -> starting number
+# end -> up to but not including
+# skip_count -> used to skip count
+#               can count forward
+#               or backward
+
+for num in range(10):
+    print(num)
+
+nums = range(1, 10, 2)
+myList = list(nums)
+for num in myList:
+    print(f"NUMBER IN LIST {num}")
