ada7e7f3408c0ceaadaa8227305562833b053893
acemouty <kyleguerrero85@gmail.com>
2020-05-18 18:42:50 -0500

    feat: created clean your room
    created an app that tells you to clean your room

---
+++ 4.Loops/excercises/clean_your_room.py
@@ -0,0 +1,12 @@
+
+def main():
+
+    print("How many times do I have to tell you???")
+
+    userInput = int(input())
+    for num in range(userInput):
+        print("CLEAN UP YOUR ROOM!")
+
+
+if __name__ == "__main__":
+    main()
