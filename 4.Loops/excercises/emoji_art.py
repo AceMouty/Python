4f86d4bd75b8bad31831acbde3aeef8d6da3bed5
acemouty <kyleguerrero85@gmail.com>
2020-05-18 23:14:32 -0500

    feat: created emoji_art
    used a while loop to create a emjoi pyramid / stair case

---
+++ 4.Loops/excercises/emoji_art.py
@@ -0,0 +1,13 @@
+
+def main():
+
+    # cretate emoji steps / pyramid
+    # using a for loop
+    counter = 1
+    while counter <= 9:
+        print("\U0001f600" * counter)
+        counter += 1
+
+
+if __name__ == "__main__":
+    main()
